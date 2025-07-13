052d6360ef45a0b60cd9e97b9cf602d4e3655846
Gonzalo Ayuso <gonzalo123@gmail.com>
2025-07-13 18:26:36 +0200

    wip

--- src/commands/check_weather.py
+++ src/commands/check_weather.py
@@ -4,7 +4,7 @@ import click
 
 from core.aws import setup_aws_conf
 from modules.weather.main import ai
-from modules.weather.prompts import PROMPT_TOOLS, SYSTEM_PROMPT
+from modules.weather.prompts import SYSTEM_PROMPT
 from settings import AWS_ASSUME_ROLE, AWS_REGION, AWS_PROFILE_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 logger = logging.getLogger(__name__)
